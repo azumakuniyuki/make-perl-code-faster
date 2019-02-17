100649e3a06eab2501226a39edb2d310cd27bf88
azumakuniyuki <azumakuniyuki@gmail.com>
2019-02-17 20:12:05 +0900

    Update variable names, Add memory size, and the results in Perl 5.18.2

--- check-long-string.pl
+++ check-long-string.pl
@@ -5,7 +5,7 @@ use warnings;
 use Benchmark ':all';
 use Test::More 'no_plan';
 
-my $String = 'Delivery failed, blacklisted by rbl.nyaan.jp';
+my $Failed = 'Delivery failed, blacklisted by rbl.nyaan.jp';
 my $RegExp = qr{(?>
      access[ ]denied[.][ ]ip[ ]name[ ]lookup[ ]failed
     |all[ ]mail[ ]servers[ ]must[ ]have[ ]a[ ]ptr[ ]record[ ]with[ ]a[ ]valid[ ]reverse[ ]dns[ ]entry
@@ -17,7 +17,7 @@ my $RegExp = qr{(?>
     |blacklisted[ ]by
     )
 }x;
-my $Fixed = [
+my $String = [
     'access denied. ip name lookup failed',
     'all mail servers must have a ptr record with a valid reverse dns entry',
     'bad sender ip address',
@@ -35,18 +35,18 @@ sub regex {
 
 sub grep1 {
     my $v = shift;
-    return 1 if grep { index($v, $_) > -1 } @$Fixed;
+    return 1 if grep { index($v, $_) > -1 } @$String;
 }
 
 sub grep2 {
     my $v = shift;
-    return 1 if grep { rindex($v, $_) > -1 } @$Fixed;
+    return 1 if grep { rindex($v, $_) > -1 } @$String;
 }
 
 sub loop1 {
     my $v = shift;
     my $p = 0;
-    for my $e ( @$Fixed ) {
+    for my $e ( @$String ) {
         next if index($v, $e) == -1;
         $p = 1;
         last;
@@ -54,21 +54,37 @@ sub loop1 {
     return $p;
 }
 
-is regex($String), 1;
-is grep1($String), 1;
-is grep2($String), 1;
-is loop1($String), 1;
+is regex($Failed), 1;
+is grep1($Failed), 1;
+is grep2($Failed), 1;
+is loop1($Failed), 1;
 
 printf("Running with Perl %s on %s\n%s\n", $^V, $^O, '-' x 80);
 cmpthese(6e5, {
-        '$v =~ //' => sub { regex($String) },
-        'grep { index }' => sub { grep1($String) },
-        'grep { rindex }' => sub { grep2($String) },
-        'for { index }' => sub { loop1($String) },
+        '$v =~ //'        => sub { regex($Failed) },
+        'grep { index }'  => sub { grep1($Failed) },
+        'grep { rindex }' => sub { grep2($Failed) },
+        'for { index }'   => sub { loop1($Failed) },
     }
 );
 
+eval { require Devel::Size };
+unless( $@ ) {
+    printf("\n");
+    printf("%s %6d bytes\n", 'qr{...}', Devel::Size::total_size($RegExp));
+    printf("%s %6d bytes\n", '[a,b,c]', Devel::Size::total_size($String));
+}
+
 __END__
+
+Running with Perl v5.18.2 on darwin
+--------------------------------------------------------------------------------
+                    Rate   $v =~ // for { index } grep { index } grep { rindex }
+$v =~ //        382166/s         --          -39%           -48%            -48%
+for { index }   625000/s        64%            --           -15%            -15%
+grep { index }  731707/s        91%           17%             --             -0%
+grep { rindex } 731707/s        91%           17%             0%              --
+
 Running with Perl v5.22.1 on darwin
 --------------------------------------------------------------------------------
                     Rate   $v =~ // for { index } grep { index } grep { rindex }
