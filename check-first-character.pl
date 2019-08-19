8444c2a27932b891ab64f6caaca34b3297e6d2d3
Lee Katz - Aspen <gzu2@cdc.gov>
2019-08-19 14:36:01 -0400

    remade the test to reflect 'what is the first character?'

--- check-first-character.pl
+++ check-first-character.pl
@@ -7,18 +7,48 @@ use Test::More 'no_plan';
 
 my $Code = '5.1.1';
 
-sub regexp { return 1 if $Code =~ /\A[45]/ }
-sub index1 { return 1 if( index($Code, '4') == 0 || index($Code, '5') == 0 ) }
-sub subst1 { return 1 if( substr($Code, 0, 1) == 5 || substr($Code, 0, 1) == 4) }
+my $re = qr/^(.)/;
+my $reNum = qr/^(\d)/;
+my @int1=(1..10);
+my @int2=reverse(@int1);
 
-is regexp, 1;
-is index1, 1;
-is subst1, 1;
+sub regexp { if($Code =~ /^(.)/){ return $1;} }
+sub regexp_num { if($Code =~ /^(\d)/){ return $1;} }
+sub regexp_compiled { if($Code =~ $re){return $1;} }
+sub regexp_comp_num { if($Code =~ $reNum){return $1;} }
+sub subst1 { return substr($Code, 0, 1); }
+sub index1 { 
+  for my $i(@int1){
+    if( index($Code, "$i") == 0){
+      return $i;
+    }
+  }
+}
+sub index2 { 
+  for my $i(@int2){
+    if( index($Code, "$i") == 0){
+      return $i;
+    }
+  }
+}
+
+my $first = substr($Code, 0, 1);
+is regexp, $first, "regexp";
+is regexp_num, $first, "regexp_num";
+is regexp_compiled, $first, "regexp_compiled";
+is regexp_comp_num, $first, "regexp_comp_num";
+is index1, $first, "index1";
+is index2, $first, "index2";
+is subst1, $first, "substr";
 
 printf("Running with Perl %s on %s\n%s\n", $^V, $^O, '-' x 80);
 cmpthese(6e6, {
-        '=~ /\A[45]/' => sub { regexp() },
-        'index(...,)' => sub { index1() },
+        '=~ /^(.)/  ' => sub { regexp() },
+        '=~ /^(\d)/ ' => sub { regexp_num() },
+        '=~ $re     ' => sub { regexp_compiled() },
+        '=~ $reNum  ' => sub { regexp_comp_num() },
+        'index1()   ' => sub { index1() },
+        'index2()   ' => sub { index2() },
         'substr(..,)' => sub { subst1() },
     }
 );
