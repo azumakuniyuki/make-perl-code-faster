3e80f7ac7b4a1b0791a2843d8b1a31a195fdc56c
azumakuniyuki <azumakuniyuki@gmail.com>
2019-08-20 12:24:32 +0900

    Add code using a variable including regular expression #2

--- check-first-character.pl
+++ check-first-character.pl
@@ -6,18 +6,22 @@ use Benchmark ':all';
 use Test::More 'no_plan';
 
 my $Code = '5.1.1';
+my $Re1c = qr/\A[45]/;
 
 sub regexp { return 1 if $Code =~ /\A[45]/ }
+sub callqr { return 1 if $Code =~ $Re1c }
 sub index1 { return 1 if( index($Code, '4') == 0 || index($Code, '5') == 0 ) }
 sub subst1 { return 1 if( substr($Code, 0, 1) == 5 || substr($Code, 0, 1) == 4) }
 
 is regexp, 1;
+is callqr, 1;
 is index1, 1;
 is subst1, 1;
 
 printf("Running with Perl %s on %s\n%s\n", $^V, $^O, '-' x 80);
 cmpthese(6e6, {
         '=~ /\A[45]/' => sub { regexp() },
+        '=~ $Re1c'    => sub { callqr() },
         'index(...,)' => sub { index1() },
         'substr(..,)' => sub { subst1() },
     }
@@ -27,14 +31,17 @@ __END__
 
 Running with Perl v5.22.1 on darwin
 --------------------------------------------------------------------------------
-                 Rate =~ /\A[45]/ substr(..,) index(...,)
-=~ /\A[45]/ 3592814/s          --         -5%         -7%
-substr(..,) 3797468/s          6%          --         -2%
-index(...,) 3870968/s          8%          2%          --
+                 Rate    =~ $Re1c =~ /\A[45]/ index(...,) substr(..,)
+=~ $Re1c    1336303/s          --        -63%        -67%        -67%
+=~ /\A[45]/ 3614458/s        170%          --        -10%        -11%
+index(...,) 4026846/s        201%         11%          --         -1%
+substr(..,) 4054054/s        203%         12%          1%          --
 
 Running with Perl v5.28.1 on darwin
 --------------------------------------------------------------------------------
-                 Rate =~ /\A[45]/ index(...,) substr(..,)
-=~ /\A[45]/ 4545455/s          --        -17%        -18%
-index(...,) 5454545/s         20%          --         -2%
-substr(..,) 5555556/s         22%          2%          --
+                 Rate    =~ $Re1c =~ /\A[45]/ index(...,) substr(..,)
+=~ $Re1c    1392111/s          --        -70%        -73%        -77%
+=~ /\A[45]/ 4615385/s        232%          --         -9%        -22%
+index(...,) 5084746/s        265%         10%          --        -14%
+substr(..,) 5940594/s        327%         29%         17%          --
+
