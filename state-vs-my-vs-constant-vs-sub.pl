bf4a471dfad7b08b1d001678f61b678c902037d1
azumakuniyuki <azumakuniyuki@gmail.com>
2020-04-24 16:41:32 +0900

    Update benchmark code

--- state-vs-my-vs-constant-vs-sub.pl
+++ state-vs-my-vs-constant-vs-sub.pl
@@ -6,52 +6,48 @@ use warnings;
 use Benchmark ':all';
 use Test::More 'no_plan';
 
-my $NEKO1           = 'nyaan';
-state $NEKO2        = 'nyaan';
-use constant NEKO3 => 'nyaan';
-sub NEKO4           { 'nyaan' }
+my $NEKO1  = 'nyaan';
+my $NYAAN1 = ['neko', 'nyaan'];
+my $CAT1   = { 'v' => 2 };
 
-sub myvariable  { my $v = shift; return $v.'-'.$NEKO1.'-'.$NEKO1 }
-sub usestate    { my $v = shift; return $v.'-'.$NEKO2.'-'.$NEKO2 }
-sub useconstant { my $v = shift; return $v.'-'.NEKO3.'-'.NEKO3 }
-sub subroutine  { my $v = shift; return $v.'-'.NEKO4.'-'.NEKO4 }
+state $NEKO2  = 'nyaan';
+state $NYAAN2 = ['neko', 'nyaan'];
+state $CAT2   = { 'v' => 2 };
 
-is myvariable('neko'), 'neko-nyaan-nyaan';
-is usestate('neko'),   'neko-nyaan-nyaan';
-is useconstant('neko'),'neko-nyaan-nyaan';
-is subroutine('neko'), 'neko-nyaan-nyaan';
+use constant NEKO3  => 'nyaan';
+use constant NYAAN3 => ['neko', 'nyaan'];
+use constant CAT3   => { 'v' => 2 };
+
+sub NEKO4  { 'nyaan' }
+sub NYAAN4 { ['neko', 'nyaan'] };
+sub CAT4   { { 'v' => 2 } };
+
+sub myvariable  { return join('-', $NEKO1, @$NYAAN1, $CAT1->{'v'}, $NYAAN1->[1]) };
+sub usestate    { return join('-', $NEKO2, @$NYAAN2, $CAT2->{'v'}, $NYAAN2->[1]) };
+sub useconstant { return join('-', NEKO3, @{ NYAAN3() }, CAT3->{'v'}, NYAAN3->[1]) };
+sub subroutine  { return join('-', NEKO4, @{ NYAAN4() }, CAT4->{'v'}, NYAAN4->[1]) };
+
+is myvariable(), 'nyaan-neko-nyaan-2-nyaan';
+is usestate(),   'nyaan-neko-nyaan-2-nyaan';
+is useconstant(),'nyaan-neko-nyaan-2-nyaan';
+is subroutine(), 'nyaan-neko-nyaan-2-nyaan';
 
 printf("Running with Perl %s on %s\n%s\n", $^V, $^O, '-' x 80);
 cmpthese(6e6, {
-        'my $v'      => sub { myvariable('neko') },
-        'state $v'   => sub { usestate('neko') },
-        'constant v' => sub { useconstant('neko') },
-        'sub {...}'  => sub { subroutine('neko') },
+        'my $v'      => sub { myvariable() },
+        'state $v'   => sub { usestate() },
+        'constant v' => sub { useconstant() },
+        'sub {...}'  => sub { subroutine() },
     }
 );
 
 __END__
 
-Running with Perl v5.22.1 on darwin
---------------------------------------------------------------------------------
-                Rate  sub {...}      my $v   state $v constant v
-sub {...}  1273885/s         --       -49%       -50%       -51%
-my $v      2510460/s        97%         --        -2%        -3%
-state $v   2553191/s       100%         2%         --        -1%
-constant v 2575107/s       102%         3%         1%         --
-
-Running with Perl v5.28.1 on darwin
---------------------------------------------------------------------------------
-                Rate  sub {...}      my $v constant v   state $v
-sub {...}  1812689/s         --       -54%       -56%       -56%
-my $v      3973510/s       119%         --        -3%        -4%
-constant v 4081633/s       125%         3%         --        -1%
-state $v   4137931/s       128%         4%         1%         --
-
 Running with Perl v5.30.0 on darwin
 --------------------------------------------------------------------------------
                 Rate  sub {...}      my $v constant v   state $v
-sub {...}  2586207/s         --       -48%       -52%       -59%
-my $v      5000000/s        93%         --        -8%       -20%
-constant v 5405405/s       109%         8%         --       -14%
-state $v   6250000/s       142%        25%        16%         --
+sub {...}   676437/s         --       -84%       -84%       -85%
+my $v      4195804/s       520%         --        -2%        -7%
+constant v 4285714/s       534%         2%         --        -5%
+state $v   4511278/s       567%         8%         5%         --
+
